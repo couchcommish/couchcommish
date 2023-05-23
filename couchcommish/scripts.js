5f86d2156447a4a4770797c48415d3724abc9f9f
Brandon Novarro <133735379+couchcommish@users.noreply.github.com>
2023-05-23 00:37:29 -0400

    Update scripts.js

--- couchcommish/scripts.js
+++ couchcommish/scripts.js
@@ -1,8 +1,4 @@
 function toggleNav() {
     var nav = document.getElementById('mobile-nav-menu');
-    if (nav.classList.contains('hidden')) {
-        nav.classList.remove('hidden');
-    } else {
-        nav.classList.add('hidden');
-    }
+    nav.classList.toggle('hidden');
 }
