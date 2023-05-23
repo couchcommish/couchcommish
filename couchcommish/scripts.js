6c5768ea216a09e168a3665be0b70bcd636253c8
Brandon Novarro <133735379+couchcommish@users.noreply.github.com>
2023-05-23 11:55:43 -0400

    Update scripts.js

--- couchcommish/scripts.js
+++ couchcommish/scripts.js
@@ -1,4 +1,16 @@
-function toggleNav() {
-    var nav = document.getElementById('mobile-nav-menu');
-    nav.classList.toggle('hidden');
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.getElementById('mobile-menu-toggle');
+    var navMenu = document.getElementById('mobile-nav-menu');
+
+    // Toggle the menu when the hamburger icon is clicked
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+
+    // Close the menu when a navigation item is clicked
+    navMenu.addEventListener('click', function(event) {
+        if (event.target.tagName === 'A') {
+            navMenu.classList.remove('active');
+        }
+    });
+});
